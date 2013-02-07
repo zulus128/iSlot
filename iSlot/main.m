e90722b1373c132b60244fb4f01ea1c9b39a77b0
Zul <Zulus@Zuls-Mac.local>
2013-02-06 21:39:16 -0800

    Initial Commit

---
+++ iSlot/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iSlot
+//
+//  Created by Zul on 2/6/13.
+//  Copyright Zul 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
