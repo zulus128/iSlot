bbc4b3f3d4b03316360fac4d2aeb20194b92a8a6
Zul <Zulus@Zuls-Mac.local>
2013-02-27 03:42:42 -0800

    fi12399vtrr884f125554tz245

--- iSlot/AppDelegate.m
+++ iSlot/AppDelegate.m
@@ -9,7 +9,7 @@
 #import "cocos2d.h"
 
 #import "AppDelegate.h"
-//#import "IntroLayer.h"
+#import "IntroLayer.h"
 #import "GameLayer.h"
 
 @implementation MyNavigationController
@@ -48,7 +48,8 @@
 	if(director.runningScene == nil) {
 		// Add the first scene to the stack. The director will draw it immediately into the framebuffer. (Animation is started automatically when the view is displayed.)
 		// and add the scene to the stack. The director will run it when it automatically when the view is displayed.
-		[director runWithScene: [GameLayer scene]];
+//		[director runWithScene: [GameLayer scene]];
+		[director runWithScene: [IntroLayer scene]];
 	}
 }
 @end
--- iSlot/IntroLayer.m
+++ iSlot/IntroLayer.m
@@ -11,7 +11,6 @@
 #import "IntroLayer.h"
 #import "GameLayer.h"
 
-
 #pragma mark - IntroLayer
 
 // HelloWorldLayer implementation
@@ -41,26 +40,52 @@
 		// ask director for the window size
 		CGSize size = [[CCDirector sharedDirector] winSize];
 
-		CCSprite *background;
+//		CCSprite *background;
+//		
+//		if( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone ) {
+//			background = [CCSprite spriteWithFile:@"Default.png"];
+//			background.rotation = 90;
+//		} else {
+//			background = [CCSprite spriteWithFile:@"Default-Landscape~ipad.png"];
+//		}
+//		background.position = ccp(size.width/2, size.height/2);
+//
+//		// add the label as a child to this Layer
+//		[self addChild: background];
+        
+/*		CCMenuItemSprite *horizontalTestItem =
+		[CCMenuItemSprite itemFromNormalSprite: [CCSprite spriteWithFile: @"PresentButton.png"]
+								selectedSprite: [CCSprite spriteWithFile: @"PresentButton.png"]
+										target: self
+									  selector: @selector(itemPressed:)];
 		
-		if( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone ) {
-			background = [CCSprite spriteWithFile:@"Default.png"];
-			background.rotation = 90;
-		} else {
-			background = [CCSprite spriteWithFile:@"Default-Landscape~ipad.png"];
-		}
-		background.position = ccp(size.width/2, size.height/2);
+		CCMenuItemSprite *priorityTestItem =
+		[CCMenuItemSprite itemFromNormalSprite: [CCSprite spriteWithFile: @"FutureButton.png"]
+								selectedSprite: [CCSprite spriteWithFile: @"FutureButton.png"]
+										target: self
+									  selector: @selector(itemPressed:)];
+		
+		// Distinguish Normal/Selected State of Menu Items.
+		[horizontalTestItem.selectedImage setColor:ccGRAY];
+		[priorityTestItem.selectedImage setColor:ccGRAY];
 
-		// add the label as a child to this Layer
-		[self addChild: background];
+        
+        
+        
+        CCMenuAdvanced *menu = [CCMenuAdvanced menuWithItems:horizontalTestItem, priorityTestItem, nil];
+//		[menu alignItemsHorizontallyWithPadding: 0.33 * horizontalTestItem.contentSize.width ];
+        [menu alignItemsHorizontally]; //< also sets contentSize and keyBindings on Mac
+
+		[self addChild: menu z:30];
+ */
 	}
 	
 	return self;
 }
 
--(void) onEnter
-{
-	[super onEnter];
-	[[CCDirector sharedDirector] replaceScene:[CCTransitionFade transitionWithDuration:1.0 scene:[GameLayer scene] ]];
-}
+//-(void) onEnter
+//{
+//	[super onEnter];
+//	[[CCDirector sharedDirector] replaceScene:[CCTransitionFade transitionWithDuration:1.0 scene:[GameLayer scene] ]];
+//}
 @end
